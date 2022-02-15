9d6c857d0bcc2f554c9812f3b32671eb7c82fdd1
CryptoChris <c.peters@makermaker.de>
2022-02-15 14:34:53 +0100

    added cors middleware and a postModel

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,11 @@ const {errorHandler} = require("./middleware/errorMiddleware");
 const PORT = process.env.PORT || 6969;
 const connectDB = require("./config/db");
 const app = express();
+const cors = require('cors')
 
 //Verbindung zur Datenbank herstellen
 connectDB()
-
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
