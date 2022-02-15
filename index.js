4301fec6da5a6899fa01c4aedff516cee3e0cb7d
CryptoChris <c.peters@makermaker.de>
2022-02-15 09:05:48 +0100

    Added some middleware

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
+const cors = require('cors')
 require("dotenv").config();
 const PORT = process.env.PORT;
+app.use(express.json())
+app.use(cors())
 
 app.get('/', (req,res) => {
 	res.json({name:'hokuspokus'})
