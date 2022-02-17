56660f1d4e42614f8a18b9abde2aa353e9e52190
StefanSeliger <Your_seligerstefan84@gmail.com>
2022-02-17 16:13:44 +0100

    logout

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const multer = require('multer')
-const upload = multer({dest:'uploads/'})
+const upload = multer({ dest: 'uploads/' })
 const router = express.Router();
 const { loginUser, registerUser, getCurrentUser } = require("../controllers/userController");
 //Um den Zugriff auf eine Route zu beschränken, protectRoute als 2. argument der jeweiligen Route hinzufügen
 const { protectRoute } = require("../middleware/authMiddleware");
 
-router.post("/",upload.single('avatar'), registerUser);
+router.post("/", upload.single('avatar'), registerUser);
 router.post("/login", loginUser);
 router.get("/currentuser", protectRoute, getCurrentUser)
+
+// Logout DELETE COOKIE
+router.get('/logout', (req, res) => {
+    //DELETING username COOKIE
+    res.clearCookie('TweetieToken');
+    // REDIRECT OT HOME
+    res.redirect('/');
+});
 
 module.exports = router;
